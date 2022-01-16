61461739054d42395cddff28b301719611f9b6bb
Travis Okonicha <thrinitee@gmail.com>
2022-01-16 02:13:52 +0100

    Reducing boiler plates with Flutter Hooks

--- lib/Aricah/Job_Seeker/Home/Drawer/new_drawer.dart
+++ lib/Aricah/Job_Seeker/Home/Drawer/new_drawer.dart
@@ -1,79 +1,61 @@
+import 'package:flutter_hooks/flutter_hooks.dart';
 import '../../../library_imports.dart';
 
-class NewDrawer extends StatefulWidget {
-  const NewDrawer({Key? key}) : super(key: key);
+class Example extends HookWidget {
+  const Example({Key? key}) : super(key: key);
 
-  @override
-  _NewDrawerState createState() => _NewDrawerState();
-}
-
-class _NewDrawerState extends State<NewDrawer>
-    with SingleTickerProviderStateMixin {
-  AnimationController? animationController;
-
-  @override
-  void initState() {
-    super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 250),
-    );
-  }
-
-  void toggle() {
-    animationController!.isDismissed
-        ? animationController!.forward()
-        : animationController!.reverse();
-  }
-
-  void _onDragStart(DragStartDetails details) {
-    double minDragStartEdge = 0.3;
-    double maxDragStartEdge = 10;
-    bool isDragOpenFromLeft = animationController!.isDismissed &&
-        details.globalPosition.dx < minDragStartEdge;
-    bool isDragCloseFromLeft = animationController!.isCompleted &&
-        details.globalPosition.dx > maxDragStartEdge;
-
-    bool canBeDragged = isDragOpenFromLeft || isDragCloseFromLeft;
-  }
-
-  void _onDragUpdate(DragUpdateDetails details) {
-    bool canBeDragged = true;
-    // bool  _canBeDragged;
-    if (canBeDragged) {
-      double delta = details.primaryDelta! / maxSlide;
-      animationController!.value += delta;
-    }
-  }
-
-  void _onDragEnd(DragEndDetails details) {
-    if (animationController!.isDismissed || animationController!.isCompleted) {
-      return;
-    }
-    if (details.velocity.pixelsPerSecond.dx.abs() >= 345) {
-      double visualVelocity = details.velocity.pixelsPerSecond.dx /
-          MediaQuery.of(context).size.width;
-
-      animationController!.fling(velocity: visualVelocity);
-    } else if (animationController!.value < 0.5) {
-      animationController!.forward(); //close();
-    } else {
-      animationController!.reverse();
-    }
-  }
-
-  final double maxSlide = 225.0;
-  var myDawer = Container(
-    color: Colors.orangeAccent,
-  );
-  // var myChild = Container(
-  //   decoration: BoxDecoration(
-  //     color: Colors.lightBlueAccent,
-  //     borderRadius: BorderRadius.circular(2 * animationController!.value;),
-  //   ),
-  // );
   @override
   Widget build(BuildContext context) {
+    final animationController = useAnimationController(
+      duration: const Duration(milliseconds: 250),
+    );
+    double maxSlide = 225.0;
+
+    void _onDragStart(DragStartDetails details) {
+      double minDragStartEdge = 0.3;
+      double maxDragStartEdge = 10;
+      bool isDragOpenFromLeft = animationController.isDismissed &&
+          details.globalPosition.dx < minDragStartEdge;
+      bool isDragCloseFromLeft = animationController.isCompleted &&
+          details.globalPosition.dx > maxDragStartEdge;
+
+      bool canBeDragged = isDragOpenFromLeft || isDragCloseFromLeft;
+    }
+
+    void _onDragUpdate(DragUpdateDetails details) {
+      bool canBeDragged = true;
+      // bool  _canBeDragged;
+      if (canBeDragged) {
+        double delta = details.primaryDelta! / maxSlide;
+        animationController.value += delta;
+      }
+    }
+
+    void _onDragEnd(DragEndDetails details) {
+      if (animationController.isDismissed || animationController.isCompleted) {
+        return;
+      }
+      if (details.velocity.pixelsPerSecond.dx.abs() >= 345) {
+        double visualVelocity = details.velocity.pixelsPerSecond.dx /
+            MediaQuery.of(context).size.width;
+
+        animationController.fling(velocity: visualVelocity);
+      } else if (animationController.value < 0.5) {
+        animationController.forward();
+      } else {
+        animationController.reverse();
+      }
+    }
+
+    void toggle() {
+      animationController.isDismissed
+          ? animationController.forward()
+          : animationController.reverse();
+    }
+
+    var myDawer = Container(
+      color: Colors.orangeAccent,
+    );
     return Scaffold(
       body: GestureDetector(
         onHorizontalDragStart: _onDragStart,
@@ -81,10 +63,10 @@ class _NewDrawerState extends State<NewDrawer>
         onHorizontalDragUpdate: _onDragUpdate,
         onTap: toggle,
         child: AnimatedBuilder(
-          animation: animationController!,
+          animation: animationController,
           builder: (BuildContext context, Widget? child) {
-            double slide = maxSlide * animationController!.value;
-            double scale = 1 - (animationController!.value * 0.3);
+            double slide = maxSlide * animationController.value;
+            double scale = 1 - (animationController.value * 0.3);
             return Stack(
               children: [
                 myDawer,
@@ -109,7 +91,7 @@ class _NewDrawerState extends State<NewDrawer>
                       //   ),
                       // ],
                       borderRadius: BorderRadius.circular(
-                        50 * animationController!.value,
+                        50 * animationController.value,
                       ),
                     ),
                   ),
